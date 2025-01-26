e763ae8ab28a9122b9e3724a8eaf7e3225e60956
Dumbelfo <dumbelfo08@gmail.com>
2025-01-26 21:00:37 +0100

    Fixed version

--- Tebas.cs
+++ Tebas.cs
@@ -21,7 +21,7 @@ class Tebas{
 	private static bool configInit;
 	private static bool localInit;
 	
-	public const string currentVersion = "v0.3";
+	public const string currentVersion = "v0.3.1";
 	
 	public static void Main(string[] args){
 		workingDirectory = Directory.GetCurrentDirectory();
